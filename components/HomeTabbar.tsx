7244f98128960092a4558a82c7e975b91d3773e4
Aaditya-shah-teli <shahsudha259@gmail.com>
2025-07-18 15:52:45 +0545

    final commit

--- components/HomeTabbar.tsx
+++ components/HomeTabbar.tsx
@@ -8,14 +8,14 @@ interface Props {
 
 const HomeTabbar = ({ selectedTab, onTabSelect }: Props) => {
   return (
-    <div className="flex items-center flex-wrap gap-5 justify-between">
+    <div className="flex items-center flex-wrap gap-5 justify-between overflow-hidden">
       <div className="flex items-center gap-1.5 text-sm font-semibold">
-        <div className="flex items-center gap-1.5 md:gap-3">
+        <div className="flex items-center gap-1.5 md:gap-3 overflow-hidden">
           {productType?.map((item) => (
             <button
               onClick={() => onTabSelect(item?.title)}
               key={item?.title}
-              className={`border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/10"}`}
+              className={`overflow-hidden border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/10"}`}
             >
               {item?.title}
             </button>
