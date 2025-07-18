afef8981e8c06533e47ba124f5ea8ebfd606a06d
Aaditya-shah-teli <shahsudha259@gmail.com>
2025-07-18 15:58:57 +0545

    final commit responsive

--- components/HomeTabbar.tsx
+++ components/HomeTabbar.tsx
@@ -8,27 +8,30 @@ interface Props {
 
 const HomeTabbar = ({ selectedTab, onTabSelect }: Props) => {
   return (
-    <div className="flex items-center flex-wrap gap-5 justify-between overflow-hidden">
-      <div className="flex items-center gap-1.5 text-sm font-semibold">
-        <div className="flex items-center gap-1.5 md:gap-3 overflow-hidden">
-          {productType?.map((item) => (
-            <button
-              onClick={() => onTabSelect(item?.title)}
-              key={item?.title}
-              className={`overflow-hidden border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/10"}`}
-            >
-              {item?.title}
-            </button>
-          ))}
-        </div>
-      </div>
-      <Link
-        href={"/shop"}
-        className="border border-darkColor px-4 py-1 rounded-full hover:bg-shop_light_green hover:text-white hover:border-shop_light_green hoverEffect"
-      >
-        See all
-      </Link>
+    <div>
+      <h2>Items</h2>
     </div>
+    // <div className="flex items-center flex-wrap gap-5 justify-between overflow-hidden">
+    //   <div className="flex items-center gap-1.5 text-sm font-semibold">
+    //     <div className="flex items-center gap-1.5 md:gap-3 overflow-hidden">
+    //       {productType?.map((item) => (
+    //         <button
+    //           onClick={() => onTabSelect(item?.title)}
+    //           key={item?.title}
+    //           className={`overflow-hidden border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/10"}`}
+    //         >
+    //           {item?.title}
+    //         </button>
+    //       ))}
+    //     </div>
+    //   </div>
+    //   <Link
+    //     href={"/shop"}
+    //     className="border border-darkColor px-4 py-1 rounded-full hover:bg-shop_light_green hover:text-white hover:border-shop_light_green hoverEffect"
+    //   >
+    //     See all
+    //   </Link>
+    // </div>
   );
 };
 
